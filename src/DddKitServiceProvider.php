3e434383fed45858bcadd979f04862e483856e5d
Harish120 <pantharish120@gmail.com>
2026-09-09 13:25:26 +1000

    ✨ feat: add DddKitServiceProvider and ddd config

---
+++ src/DddKitServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Harryes\LaravelDddKit;
+
+use Harryes\LaravelDddKit\Console\Commands\DomainMakeCommand;
+use Illuminate\Support\ServiceProvider;
+
+final class DddKitServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/ddd.php', 'ddd');
+    }
+
+    public function boot(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/ddd.php' => config_path('ddd.php'),
+        ], 'ddd-config');
+
+        $this->publishes([
+            __DIR__.'/../stubs' => base_path('stubs/ddd'),
+        ], 'ddd-stubs');
+
+        $this->commands([
+            DomainMakeCommand::class,
+        ]);
+    }
+}
